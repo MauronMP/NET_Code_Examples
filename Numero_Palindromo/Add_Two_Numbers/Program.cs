30b60112d34082235fa8cbfd1588621ba25ff927
Pablo Morenillas Pino <pmpino@atlax360.com>
2023-01-12 13:23:06 +0100

    feat(challenge #9) feat# Please enter the commit message for your changes. Lines starting

---
+++ Numero_Palindromo/Add_Two_Numbers/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Solution sol = new Solution();
+Console.WriteLine(sol.IsPalindrome(-123321));
+public class Solution
+{
+    const int DIVISOR = 10;
+    public int numCifras(int x)
+    {
+        int contNumCifras = 0;
+        if ( x != 0)
+        {
+            while (x > 0)
+            {
+                x = x / DIVISOR;
+                contNumCifras++;
+            }
+        }
+        return contNumCifras;
+    }
+
+    public int[] numero_vector(int x)
+    {
+        int[] vectorCifrasN = new int[numCifras(x)];
+        int n, i = 0;
+        while (x > 0)
+        {
+            n = x % DIVISOR; 
+            x = x / DIVISOR;
+            vectorCifrasN[i] = n;
+            i++;
+        }
+        return vectorCifrasN;
+    }
+
+    public void MostrarVector(int x) 
+    {
+        int[] v_numero = numero_vector(x);
+        for (int i = 0; i < v_numero.Length; i ++)
+        {
+            Console.WriteLine(v_numero[i]);
+        }
+    }
+    public bool IsPalindrome(int x)
+    {
+        bool palindrome = true;
+
+        if (x < 0) { palindrome = false; }
+        if (x > 0)
+        {
+            int[] v_numero = numero_vector(x);
+            int numCifras_X = numCifras(x);
+            int j = v_numero.Length-1;
+            for (int i = 0; i < v_numero.Length && palindrome == true && numCifras_X/2 > i; i++)
+            {
+                Console.WriteLine("Numero i:" + v_numero[i] + " Numero j: " + v_numero[j]);
+                if (v_numero[i] != v_numero[j]) { palindrome = false;}
+                
+                j--;
+            }
+        }
+        return palindrome;
+    }
+}
